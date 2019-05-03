238437cf297884482a04bc68e08aa9b8e686641c
naval07 <49963750+naval07@users.noreply.github.com>
2019-05-03 18:29:29 -0500

    Adelanto interfaz grafica principal

--- menuPrincipal/menu.py
+++ menuPrincipal/menu.py
@@ -8,14 +8,27 @@ raiz.resizable(width="False", height="False")
 barramenu = tk.Menu(raiz)
 raiz.config(menu = barramenu)
 #---- pestañas menu ---------------
-archivo = tk.Menu(barramenu)
-opciones = tk.Menu(barramenu)
-ayuda = tk.Menu(barramenu)
+archivo = tk.Menu(barramenu, tearoff = 0)
+opciones = tk.Menu(barramenu, tearoff = 0)
+ayuda = tk.Menu(barramenu, tearoff = 0)
 #---- cascada -----
 barramenu.add_cascade(label = "Archivo", menu = archivo)
 barramenu.add_cascade(label = "Opciones", menu = opciones)
 barramenu.add_cascade(label = "Ayuda", menu = ayuda)
-
+#-- contenidos --
+#- Archivo -
+archivo.add_command(label = "Crear plantilla")
+archivo.add_command(label = "Nuevo entrenador")
+archivo.add_command(label = "Nuevo asistente técnico")
+archivo.add_command(label = "Nuevo jugador")
+#- Opciones -
+opciones.add_command(label = "Editar jugador/es")
+opciones.add_command(label = "Editar entrenadores")
+opciones.add_command(label = "Editar asistentes")
+opciones.add_command(label = "Agregar alineamiento")
+#- Ayuda -
+ayuda.add_command(label = "Datos de la aplicacion")
+ayuda.add_command(label = "Datos de creadores")
 ##------------------------------- Imagenes ------------------
 textofoto = tk.PhotoImage(file = "textlogo.png")
 textologo = tk.Label(raiz, image = textofoto)
@@ -27,7 +40,7 @@ fondomenu = tk.Label(raiz, image = fondo)
 fondomenu.pack()
 ##------------------------------- Botones -------------------
 tk.Button(raiz, image = bgPlantilla, text = "Plantilla").place(x = 480, y = 107)
-tk.Button(raiz, image = bgAlineacion, text = "Alineacion").place(x = 312, y = 107)
+tk.Button(raiz, image = bgAlineacion, text = "Alineacion").place(x = 312, y = 105)
 tk.Button(raiz, text = "Empezar", image = bgEmpezar).place(x = 145, y = 107)
 textologo.place(x=100, y=200)
 
